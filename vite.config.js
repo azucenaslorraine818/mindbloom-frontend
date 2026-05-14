cb97ad5713f30cff69976e968fc0bc662547b626
Lorraine Azucenas <azucenaslorraine@gmail.com>
2026-05-14 23:33:56 +0800

    fix: remove vite-plugin-pwa from config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: false, // using our own manifest.webmanifest
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      }
-    })
   ]
 })
